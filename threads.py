64d0848aae3e99136e288aec2b26128998ca111a
nuh.temp <nuh.temp@gmail.com>
2015-04-05 22:33:39 -0700

    Indexer becomes a class

--- threads.py
+++ threads.py
@@ -15,7 +15,7 @@ from google.appengine.api import taskqueue
 _ACTIONS = ['add', 'remove']
 
 
-class Index(webapp2.RequestHandler):
+class IndexPage(webapp2.RequestHandler):
 
   limit = 100
   count = 0
@@ -45,26 +45,27 @@ class Index(webapp2.RequestHandler):
     self.response.write('Threads Index Page. (%s)' % now)
 
 
-class Worker(webapp2.RequestHandler):
+class Indexer(background_thread.BackgroundThread):
 
-  _indexer = None
+  def __init__(self):
+    super(Indexer, self).__init__()
+    self.daemon = True
+    self._q = taskqueue.Queue('pullq')
+    self.start()
 
-  @classmethod
-  def _AddReqHandler(cls, payloads):
+  def _AddReqHandler(self, payloads):
     for payload in payloads:
       now = time.time()
       logging.info('--> ADD payload: %s', [now, payload])
 
-  @classmethod
-  def _RemoveReqHandler(cls, payloads):
+  def _RemoveReqHandler(self, payloads):
     for payload in payloads:
       now = time.time()
       logging.info('--> REMOVE payload: %s', [now, payload])
 
-  @classmethod
-  def _ProcessReq(cls, q, tag, handler):
+  def _ProcessReq(self, tag, handler):
     try:
-      tasks = q.lease_tasks_by_tag(3600, 1000, deadline=60, tag=tag)
+      tasks = self._q.lease_tasks_by_tag(3600, 1000, deadline=60, tag=tag)
     except (taskqueue.TransientError,
             apiproxy_errors.DeadlineExceededError) as e:
       logging.exception(e)
@@ -72,35 +73,34 @@ class Worker(webapp2.RequestHandler):
 
     if tasks:
       handler([simplejson.loads(task.payload) for task in tasks])
-      q.delete_tasks(tasks)
+      self._q.delete_tasks(tasks)
 
-  @classmethod
-  def StartIndexer(cls):
+  def run(self):
     cnt = 0
-    q = taskqueue.Queue('pullq')
     while True:
       logging.info('tick %s', cnt)
       cnt += 1
-      cls._ProcessReq(q, 'add', cls._AddReqHandler)
-      cls._ProcessReq(q, 'remove', cls._RemoveReqHandler)
+      self._ProcessReq('add', self._AddReqHandler)
+      self._ProcessReq('remove', self._RemoveReqHandler)
 
       time.sleep(1)
 
+
+class Workers(webapp2.RequestHandler):
+
+  _indexer = None
+
   def get(self):
-    if self._indexer is not None and self._indexer.isAlive():
-      return
-    self._indexer = background_thread.BackgroundThread(
-        target=self.StartIndexer)
-    self._indexer.daemon = True
-    self._indexer.start()
-    logging.info('Indexer has been started: %s', id(self._indexer))
+    if self._indexer is None or not self._indexer.isAlive():
+      self._indexer = Indexer()
+      logging.info('Indexer has been started: %s', id(self._indexer))
 
 
 ROUTES = [
   routes.PathPrefixRoute('/threads', [
-    routes.RedirectRoute('/', Index, 'index', strict_slash=True),
+    routes.RedirectRoute('/', IndexPage, 'index-page', strict_slash=True),
   ]),
-  routes.RedirectRoute('/_ah/start', Worker, 'worker', strict_slash=True),
+  routes.RedirectRoute('/_ah/start', Workers, 'workers', strict_slash=True),
 ]
 
 app = webapp2.WSGIApplication(ROUTES, debug=True)
