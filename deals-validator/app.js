d9276fc7bae4033778abe1b4a110446d18d84893
nuh.temp <nuh.temp@gmail.com>
2016-01-20 21:13:30 -0800

    * Disable loading images and css; * Fake UserAgent; * Add logging helpers;

--- deals-validator/app.js
+++ deals-validator/app.js
@@ -5,6 +5,7 @@ var phantomjs = require('phantomjs');
 var webdriver = require('selenium-webdriver');
 var bodyParser = require('body-parser');
 var urlParse = require('url').parse;
+var fs = require('fs');
 
 var app = express();
 
@@ -12,9 +13,66 @@ app.use(express.static('st'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+var logPrefs = new webdriver.logging.Preferences();
+logPrefs.setLevel(webdriver.logging.Type.DRIVER,
+               webdriver.logging.Level.ALL);
+logPrefs.setLevel(webdriver.logging.Type.BROWSER,
+               webdriver.logging.Level.ALL);
+logPrefs.setLevel(webdriver.logging.Type.CLIENT,
+               webdriver.logging.Level.ALL);
+console.log('webdriver logging', logPrefs);
+
 var capabilities = webdriver.Capabilities.phantomjs();
+capabilities.setLoggingPrefs(logPrefs);
 capabilities.set('phantomjs.binary.path', phantomjs.path);
+capabilities.set('phantomjs.cli.args', [
+    '--load-images=false',
+    // '--remote-debugger-port=9000',
+]);
+capabilities.set('phantomjs.page.settings.userAgent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36');
 var driver = new webdriver.Builder().withCapabilities(capabilities).build();
+
+// this runs in browser's scope
+driver.executePhantomJS(function() {
+  this.onResourceRequested = function(requestData, request) {
+    var fs = require('fs');
+    var logFile = '/tmp/console-logs.txt';
+    fs.touch(logFile);
+    var regexes = [
+      /https?:\/\/.+?\.css/gi,
+      /https?:\/\/.+?\.amazon-adsystem\.com/gi,
+      /https?:\/\/fls-na\.amazon\.com/gi,
+      /https?:\/\/.+?[\.-]images-amazon\.com/gi,
+    ]
+    var log = function(msg) {
+      console.log(msg);
+      // try {
+      //   fs.write(logFile, msg + '\n', 'a');
+      // } catch(e) {}
+    };
+    var abort = function() {
+      log('The url of the request is matching. Aborting: ' + requestData['url']);
+      request.abort()
+    }
+
+    if (requestData['Content-Type'] == 'text/css') {
+      abort();
+    }
+
+    for (var i = 0; i < regexes.length; i++) {
+      if (regexes[i].test(requestData['url'])) {
+        abort();
+        break;
+      }
+    }
+  };
+});
+
+process.on('SIGINT', function() {
+  console.log('Shuting down PhantomJS...');
+  driver.quit();
+  process.exit();
+});
 
 var HANDLERS = {
   'www.amazon.com': getAmazonPrice,
@@ -23,28 +81,65 @@ var HANDLERS = {
 };
 
 
-function getAmazonPrice(url) {
-  driver.get(url);
-  var el = webdriver.By.xpath("//span[@id='priceblock_ourprice' or @id='priceblock_dealprice']");
-  return driver.wait(function() {
-    return driver.isElementPresent(el).then(function(presents) {
-      return (presents) ? driver.findElement(el).getText() : null;
-    });
-  }, 1000, 'Failed to find price after 1 second').then(function(strPrice) {
-    return (strPrice) ? parseFloat(strPrice.replace(/[^\d.,]+/i, '')) : null;
+function printLogs(driver) {
+  var logs = new webdriver.WebDriver.Logs(driver);
+  logs.getAvailableLogTypes().then(function(data) {
+    console.log('getAvailableLogTypes:', data);
+    if (data.indexOf(webdriver.logging.Type.BROWSER) == -1) {
+      return []
+    }
+    return logs.get(webdriver.logging.Type.BROWSER);
+  }).then(function (v) {
+    console.log('BROWSER LOGS:', v.length);
+    // v && v.length && console.log(v);
+    v.forEach(function(entry) {
+      console.log(entry.level.name + '::' + entry.timestamp + '::' + entry.message)
+    })
   });
 }
 
-function getMacysPrice(url) {
-  driver.get(url);
-  var el = webdriver.By.xpath("//div[contains(@id, 'priceInfo')]/div[contains(@class, 'standardProdPricingGroup')]/span[contains(@class, 'priceSale')]");
-  return driver.wait(function() {
-    return driver.isElementPresent(el).then(function(presents) {
-      return (presents) ? driver.findElement(el).getText() : null;
-    });
-  }, 1000, 'Failed to find price after 1 second').then(function(strPrice) {
-    return (strPrice) ? parseFloat(strPrice.replace(/[^\d.,]+/i, '')) : null;
+function printPageTitle(driver) {
+  driver.getCurrentUrl().then(function(data) {
+    console.log('getCurrentUrl:', data);
   });
+}
+
+function saveScreenshot(driver) {
+  driver.takeScreenshot().then(function(data) {
+    console.log('takeScreenshot:', data.length);
+    fs.writeFileSync('/tmp/111.png', data, 'base64');
+    console.log('Saved!');
+  });
+}
+
+function getIntValueByPath(url, xPath) {
+  driver.get(url);
+  var el = webdriver.By.xpath(xPath);
+  return driver.wait(
+      driver.isElementPresent(el),
+      1000,
+      'Failed to find price after 1 second')
+  .then(function(presents) {
+    console.log('price element found');
+    return (presents) ? driver.findElement(el).getText() : null;
+  })
+  .then(function(strValue) {
+    return (strValue) ? parseFloat(strValue.replace(/[^\d.,]+/i, '')) : null;
+  });
+}
+
+function getAmazonPrice(url) {
+  return getIntValueByPath(
+      url,
+      "//span[@id='priceblock_ourprice' or @id='priceblock_dealprice']");
+}
+
+function getMacysPrice(url) {
+  return getIntValueByPath(
+      url,
+      "//div[contains(@id, 'priceInfo')]" +
+        "/div[contains(@class, 'standardProdPricingGroup')]" +
+        "/span[contains(@class, 'priceSale')]");
 }
 
 app.route('/check')
